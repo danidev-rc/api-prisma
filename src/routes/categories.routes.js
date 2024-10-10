2ca671bdffa426215f3fe38878d5ba6c0384edea
Dani <dantesamuelrodriguez@gmail.com>
2024-10-09 22:27:34 -0500

    se agrego enpoinds

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -8,4 +8,36 @@ router.get('/categories', async (req, res) => {
   res.json(caterogies)
 })
 
+router.post('/categories', async (req, res) => {
+  const newCategory = await prisma.category.create({
+    data: req.body
+  })
+  res.json(newCategory)
+})
+
+router.get('/categories/:id', async (req, res) => {
+  const { id } = req.params
+  const category = await prisma.category.findUnique({
+    where: { id: parseInt(id) }
+  })
+  res.json(category)
+})
+
+router.patch('/categories/:id', async (req, res) => {
+  const { id } = req.params
+  const category = await prisma.category.update({
+    where: { id: parseInt(id) },
+    data: req.body
+  })
+  res.json(category)
+})
+
+router.delete('/categories/:id', async (req, res) => {
+  const { id } = req.params
+  await prisma.category.delete({
+    where: { id: parseInt(id) }
+  })
+  res.json({ message: 'Category deleted' })
+})
+
 export default router
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { prisma } from '../db.js'
+
 const router = Router()
 
 router.get('/products', async (req, res) => {
@@ -8,10 +9,35 @@ router.get('/products', async (req, res) => {
 })
 
 router.post('/products', async (req, res) => {
-  const newProduct = await prisma.product.create({
+  const newProduct = await prisma.produc.create({
     data: req.body
   })
   res.json(newProduct)
+})
+
+router.get('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await prisma.produc.findUnique({
+    where: { id: parseInt(id) }
+  })
+  res.json(product)
+})
+
+router.patch('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await prisma.produc.update({
+    where: { id: parseInt(id) },
+    data: req.body
+  })
+  res.json(product)
+})
+
+router.delete('/products/:id', async (req, res) => {
+  const { id } = req.params
+  await prisma.produc.delete({
+    where: { id: parseInt(id) }
+  })
+  res.json({ message: 'Product deleted' })
 })
 
 export default router
